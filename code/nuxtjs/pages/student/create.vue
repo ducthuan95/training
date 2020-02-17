7de6a673887020ebecbbc29de70fd16f2539abed
thuantd <thuantd8451@co-well.com.vn>
2020-02-17 18:01:49 +0700

    day5

--- code/nuxtjs/pages/student/create.vue
+++ code/nuxtjs/pages/student/create.vue
@@ -1,55 +1,85 @@
 <template>
   <div>
     <div class="container">
-      <div class="row">
-        <form>
-          <div class="form-group">
-            <label>Student code</label>
-            <input type="text" class="form-control" v-model="student.code">
+      <form>
+        <div class="form-group">
+          <label>Student code <small class="text-danger">*</small></label>
+          <input v-model="student.code"
+                 type="text" class="form-control">
+        </div>
+        <div class="form-group">
+          <label>Name <small class="text-danger">*</small></label>
+          <input v-model="student.name" :rules="rules.nameRules" type="text" class="form-control">
 
-            <label>Name</label>
-            <input type="text" class="form-control" v-model="student.name">
+        </div>
+        <div class="form-group">
+          <label>Age <small class="text-danger">*</small></label>
+          <input v-model="student.age" type="text" class="form-control">
 
-            <label>Age </label>
-            <input type="text" class="form-control" v-model="student.age">
+        </div>
+        <div class="form-group">
+          <label>Gender <small class="text-danger">*</small></label>
+          <select v-model="student.gender" class="form-control">
+            <option value="">-- Select --</option>
+            <option value="1">Male</option>
+            <option value="0">Female</option>
+          </select>
 
-            <lable>Gender</lable>
-            <select name="" id="">
-              <option value="">--Select--</option>
-              <option value="1">Nam</option>
-              <option value="0">Nữ</option>
-            </select>
+        </div>
+        <div class="form-group">
+          <label>Email <small class="text-danger">*</small></label>
+          <input v-model="student.email" :rules="rules.emailRules" type="text" class="form-control">
+          <lable :message="rules.emailRules"></lable>
+        </div>
+        <div class="form-group">
+          <label>Address <small class="text-danger">*</small></label>
+          <input v-model="student.address" type="text"
+                 class="form-control">
 
-            <label>Email </label>
-            <input type="text" class="form-control" v-model="student.email">
+        </div>
 
-            <label>Address </label>
-            <input type="text" class="form-control" v-model="student.address">
-          </div>
-
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-      </div>
+        <NLink class="btn btn-outline-primary" :to="{name: 'student'}">
+          <i class="fa fa-chevron-left"></i> Back
+        </NLink>
+        <button type="button" class="btn btn-primary" @click="addStudent">
+          <i class="fa fa-save"></i> Save
+        </button>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
+
   export default {
     data() {
       return {
-        student: {
-          code: '',
-          name: '',
-          age: '',
-          email: '',
-          address: ''
-        }
+        student: {},
+        rules: {
+          nameRules:[
+            v => !!v || 'Name is required',
+            v => (v && v.length <= 20) || 'Name must be less than 20 characters',
+            v => (v && v.length >= 3) || 'Name must be at least 3 characters',
+          ],
+          emailRules:[
+            v => !!v || 'Email is required',
+            v => /.+@.+/.test(v) || 'E-mail must be valid'
+          ]
+        },
+        valid:false
+      }
+    },
+    methods: {
+      async addStudent() {
+        await this.$axios.$post(`api/student`, this.student).then((data) => {
+          this.$router.push({name: 'student', params: {message: 'Create student success.'}})
+        }).catch((error) => {
+
+        });
       }
     }
   }
 </script>
 
 <style scoped>
-
 </style>
--- code/nuxtjs/pages/student/index.vue
+++ code/nuxtjs/pages/student/index.vue
@@ -2,13 +2,16 @@
   <div class="row">
     <div class="col-md-12">
       <div class="grid simple">
-        <div  class="grid-title no-border">
+        <div class="grid-title no-border">
           <h2>Student list</h2>
         </div>
         <div>
-          <NLink :to="{name: 'index'}"><button class="btn btn-outline-primary">Home Page</button></NLink>
+          <NLink :to="{name: 'index'}">
+            <button class="btn btn-outline-primary"><i class="fa fa-home"></i>Home Page</button>
+          </NLink>
           <NLink :to="{name: 'student-create'}">
-            <button class="btn btn-outline-primary">Add new student</button></NLink>
+            <button class="btn btn-outline-primary"><i class="fa fa-plus"></i>Add new student</button>
+          </NLink>
         </div>
       </div>
       <div>
@@ -22,6 +25,7 @@
             <th>Gender</th>
             <th>Email</th>
             <th>Address</th>
+            <th></th>
           </tr>
           </thead>
           <tbody>
@@ -30,9 +34,16 @@
             <td>{{item.code}}</td>
             <td>{{item.name}}</td>
             <td>{{item.age}}</td>
-            <td>{{item.gender}}</td>
+            <td>
+              <i v-if="item.gender == 1" class="fa fa-mars"></i>
+              <i v-if="item.gender == 0" class="fa fa-venus"></i>
+            </td>
             <td>{{item.email}}</td>
             <td>{{item.address}}</td>
+            <td>
+              <i class="fa fa-edit text-dark"></i>
+              <a @click="Modal(item.id)"><i class="fa fa-trash text-danger"></i></a>
+            </td>
           </tr>
           </tbody>
         </table>
@@ -47,6 +58,26 @@
     async asyncData({$axios}) {
       const response = await $axios.$get('api/student')
       return {response}
+    },
+    methods: {
+      Modal(id) {
+        this.$bvModal.msgBoxConfirm('Are you sure?')
+          .then(value => {
+            if (value) {
+              this.deleteStudent(id)
+            }
+          })
+          .catch(err => {
+          })
+      },
+      deleteStudent() {
+        axios.$delete('api/student/${this.id}').then(res => {
+          this.message = 'Delete student success.';
+        })
+          .catch((err) => {
+            console.log(err)
+          })
+      }
     },
     data() {
       return {
